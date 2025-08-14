cea302ee3effdd27bac4eecc0e9cd0d1d983e6e7
COE_pak <pearlaboagye8@gmail.com>
2025-08-14 18:26:51 +0000

    Update working_with_lambda.py

--- working_with_lambda.py
+++ working_with_lambda.py
@@ -1,6 +1,6 @@
 database = {
-    "Alice": {"Comm Skills": 85, "Algebra": 90, "TD": 88, "Total": 263, "Average": 87.67, "Grade": "A"},
-    "Bob": {"Comm Skills": 70, "Algebra": 75, "TD": 72, "Total": 217, "Average": 72.33, "Grade": "B"},
+    "Pearl": {"Comm Skills": 85, "Algebra": 90, "TD": 88, "Total": 263, "Average": 87.67, "Grade": "A"},
+    "Kojo": {"Comm Skills": 70, "Algebra": 75, "TD": 72, "Total": 217, "Average": 72.33, "Grade": "B"},
     "Charlie": {"Comm Skills": 60, "Algebra": 65, "TD": 62, "Total": 187, "Average": 62.33, "Grade": "C"}
 }
 
@@ -10,3 +10,4 @@ print(database.items())
 for student in database.items():
     print(student[0], "-> lambda output:",
           (lambda s: s[1]["Average"])(student))
+
